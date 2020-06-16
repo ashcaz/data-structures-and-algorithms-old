93db45c6629cd3896922a8da945d9f49266d3244
Ashley Casimir <ashcas@me.com>
2020-06-15 22:56:44 -0700

    question 3 complete

--- code-challenges/301n18/split-join-slice-splice/challenges-05.test.js
+++ code-challenges/301n18/split-join-slice-splice/challenges-05.test.js
@@ -77,7 +77,8 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  const newArr = arr.split('');
+  return newArr;
 };
 
 
@@ -263,7 +264,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return an array of individual letters', () => {
     expect(wordsToCharList('Gregor')).toStrictEqual(['G', 'r', 'e', 'g', 'o', 'r']);
     expect(wordsToCharList('Gregor').length).toStrictEqual(6);
