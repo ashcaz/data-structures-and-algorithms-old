8b6cb828833cc0842bd1ae10b0d8eae3a81f9914
Ashley Casimir <ashcas@me.com>
2020-06-29 22:46:55 -0700

    complete question 2

--- code-challenges/301n18/Chaining/challenges-10.test.js
+++ code-challenges/301n18/Chaining/challenges-10.test.js
@@ -48,7 +48,16 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  let counting = input.reduce( (acc, arr, idx) => {
+    let num = arr.reduce( (acc, val) => {
+      if (val === target){
+        acc++;
+      }
+      return acc;
+    }, 0);
+    return num + acc;
+  }, 0);
+  return counting;
 };
 
 /* ------------------------------------------------------------------------------------------------
