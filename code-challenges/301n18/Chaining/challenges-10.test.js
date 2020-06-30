d2462c2def0d1a69170966afbee9ad4d142abd92
Ashley Casimir <ashcas@me.com>
2020-06-29 22:55:39 -0700

    question 3 complete

--- code-challenges/301n18/Chaining/challenges-10.test.js
+++ code-challenges/301n18/Chaining/challenges-10.test.js
@@ -71,7 +71,14 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  let counting = input.reduce( (acc, arr, idx) => {
+    let num = arr.reduce( (acc, val) => {
+      acc = acc + val;
+      return acc;
+    }, 0);
+    return num + acc;
+  }, 0);
+  return counting;
 };
 
 /* ------------------------------------------------------------------------------------------------
